2bbc8a93f1d4a792560bf7ff824d4e1ac0976787
kacper <kacper.tarlowski20@gmail.com>
2021-12-31 02:49:33 +0100

    add db tables

--- app/models.py
+++ app/models.py
@@ -3,14 +3,19 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=20), nullable=False, unique=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    services = db.relationship('UserService', backref='user', lazy=True)
 
 
 class Service(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    password = db.Column(db.String(length=30), nullable=False, unique=False)
+    name = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    users = db.relationship('UserService', backref='service', lazy=True)
 
-# class UserService(db.Model):
-#     user_id = db.Column(db.Integer())
-#     service_id = db.Column(db.Integer())
+
+class UserService(db.Model):
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), primary_key=True)
+    service_id = db.Column(db.Integer(), db.ForeignKey('service.id'), primary_key=True)
